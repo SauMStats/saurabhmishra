05000b762113ca67c61301f03cc2437ad623ba87
SauMStats <tryphd2021@gmail.com>
2025-08-08 11:56:44 +0530

    Configure base path and push build files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/saurabhmishra/",  // <-- add this line here
   plugins: [
     react(),
     runtimeErrorOverlay(),
